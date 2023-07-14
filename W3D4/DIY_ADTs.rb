41197e99231f8420848365dfedfdb9e12504d227
tanziti <tanzitian3@gmail.com>
2023-07-14 13:09:57 -0400

    tried to understand solution

--- W3D4/DIY_ADTs.rb
+++ W3D4/DIY_ADTs.rb
@@ -1,18 +1,54 @@
 class Stack
   def initialize
     # create ivar to store stack here!
-    
+    new_array = []
   end
 
   def push(el)
-    # adds an element to the stack
+    new_array.push(el)
+    el
   end
 
   def pop
-    # removes one element from the stack
+   new_array.pop # removes one element from the stack
   end
 
   def peek
-    # returns, but doesn't remove, the top element in the stack
+   new_array.last # returns, but doesn't remove, the top element in the stack
   end
 end
+
+class Queue
+
+  def initialize
+    new_array = []
+
+  end
+
+  def enqueue(el)
+    @new_array.push(el)
+    el
+  end
+
+  def dequeue
+    @new_array.shift
+  end
+
+  def peek
+    @new_array.first
+  end
+
+  private
+  attr_reader :new_array
+end
+
+# p queue = Queue.new 
+# p queue.enqueue(3)
+# p queue.enqueue(4)
+# p queue.dequeue
+# p queue.enqueue(7)
+# p queue.peek#  array = [1,2,3,4,5,6,7]
+#  array.dequeue
+#  array.queue(8)
+#  array.dequeue
+#  array.peek
